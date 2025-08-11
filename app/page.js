fd6422981647c69d91d9b029f1e62c758574da20
Kolter599 <sebastian@invisu.dk>
2025-08-11 20:50:30 +0200

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Hello from Next.js on Vercel 🎉</h1>
-      <p>If you see this, the build is good. Next step: add /trefadder/dashboard.</p>
-    </div>
-  );
-}
