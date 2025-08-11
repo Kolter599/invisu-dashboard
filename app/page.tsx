52f51c4e5f1dcde4e540173c35611bf82c1d3542
Kolter599 <sebastian@invisu.dk>
2025-08-11 20:00:32 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Invisu Dashboard App</h1>
+      <p>Deployed. Your dashboard will live at /trefadder/dashboard</p>
+    </div>
+  );
+}
